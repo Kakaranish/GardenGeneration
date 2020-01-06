760580ecad57dcfc3c4bc68eb48e08222a782940
Stanislaw Gruz <stasiekgruz@gmail.com>
2020-01-06 22:46:43 +0100

    Water path generation works fine

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     const tileSize = 32;
-    const tilesNum = 15;
+    const tilesNum = 20;
     const canvasSize = tilesNum * tileSize;
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
@@ -109,9 +109,6 @@ $(document).ready(function () {
                     let tile_type_below = getTileType(child_x, child_y + 1);
                     let tile_type_on_left = getTileType(child_x - 1, child_y);
                     let tile_type_on_right = getTileType(child_x + 1, child_y);
-                    console.log(tile_type_below);
-                    console.log(tile_type_on_left);
-                    console.log(tile_type_on_right);
                     if (tile_type_below || tile_type_on_left || tile_type_on_right) {
                         isLegal = false;
                     }
@@ -132,10 +129,7 @@ $(document).ready(function () {
                         isLegal = false;
                     }
                 }
-                else{
-                    
-                }
-                
+
                 if (isLegal == false) {
                     console.log("Illegal water path.");
                     return null;
@@ -143,7 +137,6 @@ $(document).ready(function () {
             }
 
             let child = new Tile(this, tileType, child_x, child_y);
-
             return child;
         }
 
@@ -180,6 +173,73 @@ $(document).ready(function () {
         }
     }
 
+    function generateWaterPath() {
+        const break_size = 3;
+        let init_y = Math.floor(Math.random() * tilesNum) + 1;
+        let breaks_num = Math.floor(Math.random() * 5) + 2;
+        let max_movement = Math.floor(tilesNum / breaks_num) + 2;
+
+        let remaining_movement = tilesNum;
+        let horizontal_movements = [];
+
+        console.log("Breaks num = " + breaks_num);
+        console.log("Max movement = " + max_movement);
+        for (let i = 1; i <= breaks_num; i++) {
+            if (i == breaks_num) {
+                horizontal_movements.push(remaining_movement);
+                break;
+            }
+
+            let reserved_movements_capacity = (breaks_num - i) * break_size;
+            let movement = 0;
+            do {
+                movement = Math.floor(Math.random() * (max_movement - break_size))
+                    + break_size;
+            } while (remaining_movement - movement < reserved_movements_capacity);
+            horizontal_movements.push(movement);
+            remaining_movement -= movement;
+        }
+
+        console.log(horizontal_movements);
+
+        let rootTile = new Tile(null, TileType.WATER, 1, init_y);
+        let prevTile = rootTile;
+
+        for (let i = 0; i < horizontal_movements.length; i++) {
+            let movements_num = horizontal_movements[i];
+            let j = i == 0 ? 2 : 1;
+            for (j; j <= movements_num; j++) {
+                let newTile = null;
+                do {
+                    newTile = prevTile.addChild(TileType.WATER, Direction.RIGHT);
+                } while (newTile == null);
+                prevTile = newTile;
+            }
+
+            let isMovementUp = Math.round(Math.random());
+            let maxYMovement = 0;
+            if (isMovementUp) {
+                maxYMovement = prevTile.y - 1;
+                if (maxYMovement < 1) {
+                    isMovementUp = false;
+                    maxYMovement = tilesNum - prevTile.y;
+                }
+            } else {
+                maxYMovement = tilesNum - prevTile.y;
+                if (maxYMovement < 1) {
+                    isMovementUp = true;
+                    maxYMovement = prevTile.y - 1;
+                }
+            }
+
+            let movements = Math.floor(Math.random() * (maxYMovement - 1)) + 1;
+            for (let j = 0; j < movements; j++) {
+                let movementDirection = isMovementUp ? Direction.UP : Direction.DOWN;
+                prevTile = prevTile.addChild(TileType.WATER, movementDirection);
+            }
+        }
+    }
+
     function initSampleMap() {
         let rootTile = new Tile(null, TileType.WATER, 1, 7);
         rootTile.addChild(TileType.WATER, Direction.RIGHT)
@@ -190,38 +250,38 @@ $(document).ready(function () {
             .addChild(TileType.WATER, Direction.DOWN)
             .addChild(TileType.WATER, Direction.DOWN)
             .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.UP)
+            .addChild(TileType.WATER, Direction.UP)
+            .addChild(TileType.WATER, Direction.UP)
+            .addChild(TileType.WATER, Direction.UP)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.UP)
+            .addChild(TileType.WATER, Direction.UP)
+            .addChild(TileType.WATER, Direction.UP)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.RIGHT)
             .addChild(TileType.WATER, Direction.DOWN)
-            // .addChild(TileType.WATER, Direction.UP);
-        // .addChild(TileType.WATER, Direction.RIGHT);
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.UP)
-        // .addChild(TileType.WATER, Direction.UP)
-        // .addChild(TileType.WATER, Direction.UP)
-        // .addChild(TileType.WATER, Direction.UP)
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.UP)
-        // .addChild(TileType.WATER, Direction.UP)
-        // .addChild(TileType.WATER, Direction.UP)
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.DOWN)
-        // .addChild(TileType.WATER, Direction.DOWN)
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.RIGHT)
-        // .addChild(TileType.WATER, Direction.UP)
-        // .addChild(TileType.WATER, Direction.UP)
-        // .addChild(TileType.WATER, Direction.RIGHT);
-
-        // let waterTile1 = tiles[8 - 1][6 - 1];
-        // let tree1 = waterTile1.addChild(TileType.TREE, Direction.UP);
-        // tree1.addChild(TileType.TREE, Direction.LEFT).addChild(TileType.TREE, Direction.DOWN);
-        // tree1.addChild(TileType.TREE, Direction.RIGHT);
+            .addChild(TileType.WATER, Direction.DOWN)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.RIGHT)
+            .addChild(TileType.WATER, Direction.UP)
+            .addChild(TileType.WATER, Direction.UP)
+            .addChild(TileType.WATER, Direction.RIGHT);
+        
+        let waterTile1 = tiles[8 - 1][6- 1];
+        let tree1 = waterTile1.addChild(TileType.TREE, Direction.UP);
+        tree1.addChild(TileType.TREE, Direction.LEFT).addChild(TileType.TREE, Direction.DOWN);
+        tree1.addChild(TileType.TREE, Direction.RIGHT);    
     }
-    initSampleMap();
+
+    // initSampleMap();
+    generateWaterPath();
     drawTiles();
     showGrid();
 });
